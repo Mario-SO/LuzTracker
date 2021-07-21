2766e670272f3e660cbcdefa063eacdaa46ad18c
Mario-SO <mariodevmail1010@gmail.com>
2021-07-22 00:21:06 +0200

    Much better 👍

--- main.js
+++ main.js
@@ -1,150 +1,34 @@
 let currentTime = new Date().getHours();
 
+const horaSuperValle = [0, 1, 2, 3, 4, 5, 6, 7]
+const horaValle = [8, 9, 14, 15, 16, 17, 22, 23]
+const horaPunta = [10, 11, 12, 13, 18, 19, 20, 21]
+
 let data = fetch("data.json")
     .then(response => response.json())
     .then(json => {
-        if (currentTime >= 0 & currentTime < 1) {
-            document.getElementById('price').innerText = json[0].price
-            document.getElementById('next_price').innerText = json[1].price
+
+        document.getElementById('price').innerText = json[currentTime % 24].price
+        document.getElementById('next_price').innerText = json[(currentTime + 1) % 24].price
+
+        if (horaSuperValle.includes(currentTime)) {
             document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
+        }
+        else if (horaValle.includes(currentTime)) {
+            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
+        }
+        else {
+            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
+        }
+
+        if (horaSuperValle.includes(currentTime + 1)) {
             document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
         }
-        else if (currentTime >= 1 & currentTime < 2) {
-            document.getElementById('price').innerText = json[1].price
-            document.getElementById('next_price').innerText = json[2].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 2 & currentTime < 3) {
-            document.getElementById('price').innerText = json[2].price
-            document.getElementById('next_price').innerText = json[3].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 3 & currentTime < 4) {
-            document.getElementById('price').innerText = json[3].price
-            document.getElementById('next_price').innerText = json[4].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 4 & currentTime < 5) {
-            document.getElementById('price').innerText = json[4].price
-            document.getElementById('next_price').innerText = json[5].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 5 & currentTime < 6) {
-            document.getElementById('price').innerText = json[5].price
-            document.getElementById('next_price').innerText = json[6].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 6 & currentTime < 7) {
-            document.getElementById('price').innerText = json[6].price
-            document.getElementById('next_price').innerText = json[7].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
-        else if (currentTime >= 7 & currentTime < 8) {
-            document.getElementById('price').innerText = json[7].price
-            document.getElementById('next_price').innerText = json[8].price
-            document.getElementById('flag').innerText = "🟢 Hora super-valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 8 & currentTime < 9) {
-            document.getElementById('price').innerText = json[8].price
-            document.getElementById('next_price').innerText = json[9].price
+        else if (horaValle.includes(currentTime + 1)) {
             document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
         }
-        else if (currentTime >= 9 & currentTime < 10) {
-            document.getElementById('price').innerText = json[9].price
-            document.getElementById('next_price').innerText = json[10].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 10 & currentTime < 11) {
-            document.getElementById('price').innerText = json[10].price
-            document.getElementById('next_price').innerText = json[11].price
+        else {
             document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
         }
-        else if (currentTime >= 11 & currentTime < 12) {
-            document.getElementById('price').innerText = json[11].price
-            document.getElementById('next_price').innerText = json[12].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 12 & currentTime < 13) {
-            document.getElementById('price').innerText = json[12].price
-            document.getElementById('next_price').innerText = json[13].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 13 & currentTime < 14) {
-            document.getElementById('price').innerText = json[13].price
-            document.getElementById('next_price').innerText = json[14].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 14 & currentTime < 15) {
-            document.getElementById('price').innerText = json[14].price
-            document.getElementById('next_price').innerText = json[15].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 15 & currentTime < 16) {
-            document.getElementById('price').innerText = json[15].price
-            document.getElementById('next_price').innerText = json[16].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 16 & currentTime < 17) {
-            document.getElementById('price').innerText = json[16].price
-            document.getElementById('next_price').innerText = json[17].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 17 & currentTime < 18) {
-            document.getElementById('price').innerText = json[17].price
-            document.getElementById('next_price').innerText = json[18].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 18 & currentTime < 19) {
-            document.getElementById('price').innerText = json[18].price
-            document.getElementById('next_price').innerText = json[19].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 19 & currentTime < 20) {
-            document.getElementById('price').innerText = json[19].price
-            document.getElementById('next_price').innerText = json[20].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 20 & currentTime < 21) {
-            document.getElementById('price').innerText = json[20].price
-            document.getElementById('next_price').innerText = json[21].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🔴 Hora punta 🔴"
-        }
-        else if (currentTime >= 21 & currentTime < 22) {
-            document.getElementById('price').innerText = json[21].price
-            document.getElementById('next_price').innerText = json[22].price
-            document.getElementById('flag').innerText = "🔴 Hora punta 🔴"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 22 & currentTime < 23) {
-            document.getElementById('price').innerText = json[22].price
-            document.getElementById('next_price').innerText = json[23].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora valle 🟢"
-        }
-        else if (currentTime >= 23 & currentTime < 24) {
-            document.getElementById('price').innerText = json[23].price
-            document.getElementById('next_price').innerText = json[0].price
-            document.getElementById('flag').innerText = "🟢 Hora valle 🟢"
-            document.getElementById('next_flag').innerText = "🟢 Hora super-valle 🟢"
-        }
+        console.log(currentTime, currentTime + 1)
     })
